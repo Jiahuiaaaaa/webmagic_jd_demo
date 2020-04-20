a14511e3ade4617513d8fa39b9c95bc6c2f8fe6c
chenhang <18730051277@163.com>
2020-04-20 15:53:49 +0800

    test

--- src/main/java/com/jiahui/demo/controller/ItemController.java
+++ src/main/java/com/jiahui/demo/controller/ItemController.java
@@ -2,7 +2,7 @@ package com.jiahui.demo.controller;
 
 import com.jiahui.demo.component.JdSpider;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,7 +10,7 @@ public class ItemController {
     @Autowired
     private JdSpider jdSpider;
 
-    @PostMapping("/doMain")
+    @GetMapping("/doMain")
     public  String doMain(){
         jdSpider.start();
         return  "success!";
