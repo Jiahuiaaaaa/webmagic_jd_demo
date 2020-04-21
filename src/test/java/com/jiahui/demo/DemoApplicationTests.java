065556cb48dd61d7fb7749ae94257cb0412340db
chenhang <18730051277@163.com>
2020-04-21 09:43:38 +0800

    test

--- src/test/java/com/jiahui/demo/DemoApplicationTests.java
+++ src/test/java/com/jiahui/demo/DemoApplicationTests.java
@@ -1,13 +1,21 @@
 package com.jiahui.demo;
 
+import com.jiahui.demo.component.JdSpider;
+import com.jiahui.demo.controller.ItemController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DemoApplicationTests {
 
+//    @Autowired
+//    private ItemController itemController;
+@Autowired
+private JdSpider jdSpider;
     @Test
     void contextLoads() {
+//        jdSpider.start();
     }
 
 }
